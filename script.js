d75d695e4718358eb0acaa42673ccba9e34f7337
Shekhawatbhawanisingh <shekhawatbhwanisingh49@gmail.com>
2024-12-03 19:25:59 +0530

    Calculator Using JS

---
+++ script.js
@@ -0,0 +1,32 @@
+
+let buttons = document.querySelectorAll("button");
+let input = document.querySelector("input");
+let ans = "";
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    let character = button.innerText;
+
+    if (character === "AC") {
+      ans = "";
+    } 
+    
+    else if (character === "=") {
+      
+        ans = eval(ans); 
+      
+    }
+    else if (character === "Del") {
+      ans = ans.slice(0, -1);
+    } 
+    else {
+      ans += character;
+    }
+
+    if (ans === "") {
+      input.value = "0";
+    } else {
+      input.value = ans;
+    }
+  });
+});
